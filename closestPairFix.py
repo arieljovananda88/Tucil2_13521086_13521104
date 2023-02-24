2afe4eb5aafe2829f741f0151ed290032f77903f
Ariel Jovananda <arieljovananda@Ariels-MacBook-Pro.local>
2023-02-24 16:38:45 +0700

    added bruteForce func and findClosestPair func (buggy)

--- closestPairFix.py
+++ closestPairFix.py
@@ -20,7 +20,7 @@ def closestPair(List,n1):
     
     return
 
-def sortList(lst):
+def sortListX(lst):
     if len(lst) <= 1:
         return lst
     acuan = lst[0]
@@ -31,7 +31,67 @@ def sortList(lst):
             left.append(i)
         else:
             right.append(i)
-    return sortList(left) + [acuan] + sortList(right)
+    return sortListX(left) + [acuan] + sortListX(right)
+
+def sortListY(lst):
+    if len(lst) <= 1:
+        return lst
+    acuan = lst[0]
+    left = []
+    right = []
+    for i in lst[1:]:
+        if i.y < acuan.y:
+            left.append(i)
+        else:
+            right.append(i)
+    return sortListY(left) + [acuan] + sortListY(right)
+
+def bruteForce(lst):
+    minDistance = float('inf')
+    pairTerdekat = None
+    for i in range (len(lst)):
+        for j in range(i+1, len(lst)):
+            if distance(lst[i], lst[j]) < minDistance:
+                minDistance = distance(lst[i], lst[j])
+                pairTerdekat = (lst[i], lst[j])
+    return pairTerdekat, minDistance
+
+def findClosestPair(xSorted, ySorted):
+    if len(xSorted) <= 3:
+        return bruteForce(xSorted)
+    middle = len(xSorted) // 2
+    xLeft = xSorted[:middle]
+    xRight = xSorted[middle:]
+    middleXpoint = xSorted[middle]
+    ySortedLeft = []
+    ySortedRight = []
+    for i in ySorted:
+        if i.x < middleXpoint.x :
+            ySortedLeft.append(i)
+        else:
+            ySortedRight.append(i)
+    (leftClosestPair, leftMinDist) = findClosestPair(xLeft, ySortedLeft)
+    (rightClosestPair, rightMinDist) = findClosestPair(xRight, ySortedRight)
+    minDist = min(leftMinDist, rightMinDist)
+    betweenHalves = []
+    for i in ySorted:
+        if abs(i.x - middleXpoint.x) < minDist:
+            betweenHalves.append(i)
+    closestPairBetween = None
+    for i in range (len(betweenHalves)):
+        for j in range(i+1, min(i+7, len(betweenHalves))):
+            if distance(betweenHalves[i], betweenHalves[j]) < minDist:
+                minDist = distance(betweenHalves[i], betweenHalves[j])
+                closestPairBetween = (betweenHalves[i], betweenHalves[j])
+    if closestPairBetween is not None:
+        return closestPairBetween
+    if leftMinDist < rightMinDist:
+        return leftClosestPair
+    else:
+        return rightClosestPair
+
+
+
 
 ListPoint=[]
 n=int(input("Masukkan Jumlah Titik : "))
@@ -48,9 +108,23 @@ for i in range(n):
 
 #print(closestPair(ListPoint,n))
 #print(len(ListPoint))
-sortedList = sortList(ListPoint)
+sortedListX = sortListX(ListPoint)
+sortedListY = sortListY(ListPoint)
 
 
+print("sorted on x axis")
 for i in range(n) :
-    print(sortedList[i])
+    print(sortedListX[i])
+
+
+(p1, p2) = findClosestPair(sortedListX, sortedListY)
+print(p1,p2)
+print(distance(p1,p2))
+
+
+
+#print("sorted on y axis")
+#for i in range(n) :
+    #print(sortedListY[i])
+    
 
