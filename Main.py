895c5a5b7399abc898fa773a37438890c8982a26
zaydanA <16521131@std.stei.itb.ac.id>
2023-02-26 09:00:24 +0700

    update main

--- Main.py
+++ Main.py
@@ -2,22 +2,34 @@ import random
 import time
 import ClosestPair as cp
 import visualizer as vis
+from colorama import Fore
+def main():
+    print(f"""{Fore.GREEN}
+     ██████╗██╗      ██████╗ ███████╗███████╗███████╗████████╗    ██████╗  █████╗ ██╗██████╗     ██████╗       ██████╗ 
+    ██╔════╝██║     ██╔═══██╗██╔════╝██╔════╝██╔════╝╚══██╔══╝    ██╔══██╗██╔══██╗██║██╔══██╗    ╚════██╗      ██╔══██╗
+    ██║     ██║     ██║   ██║███████╗█████╗  ███████╗   ██║       ██████╔╝███████║██║██████╔╝     █████╔╝█████╗██║  ██║
+    ██║     ██║     ██║   ██║╚════██║██╔══╝  ╚════██║   ██║       ██╔═══╝ ██╔══██║██║██╔══██╗     ╚═══██╗╚════╝██║  ██║
+    ╚██████╗███████╗╚██████╔╝███████║███████╗███████║   ██║       ██║     ██║  ██║██║██║  ██║    ██████╔╝      ██████╔╝
+     ╚═════╝╚══════╝ ╚═════╝ ╚══════╝╚══════╝╚══════╝   ╚═╝       ╚═╝     ╚═╝  ╚═╝╚═╝╚═╝  ╚═╝    ╚═════╝       ╚═════╝ 
+                                                                                                                    
+        {Fore.WHITE}  """)  
 
-  
-ListPoint=[]
-n=int(input("Masukkan Jumlah Titik : "))
-st = time.time()    
+    input(f"Press {Fore.RED}Enter{Fore.WHITE} to start...\n")
+    ListPoint=[]
+    n=int(input("Masukkan Jumlah Titik\t: "))
+    st = time.time()    
 
-for i in range(n) :
-    x=random.uniform(0,100)
-    y=random.uniform(0,100)
-    z=random.uniform(0,100)
-    ListPoint.append(cp.Point(x,y,z))
-Result=cp.closestPair(ListPoint)   
-print(Result)
-print('EucDistance count :',cp.count) 
-RunTime = (time.time() - st)
-print('Execution time:', RunTime, 'seconds') 
-vis.visualizer(ListPoint,Result[1])
-# for i in range(n) :
-#     print(ListPoint[i])
+    for i in range(n) :
+        x=random.uniform(0,100)
+        y=random.uniform(0,100)
+        z=random.uniform(0,100)
+        ListPoint.append(cp.Point(x,y,z))
+    Result=cp.closestPair(ListPoint)   
+    print(Result)
+    print('EuclideanDistance count\t:',cp.count) 
+    RunTime = (time.time() - st)
+    print('Execution time\t\t:', RunTime, 'seconds') 
+    vis.visualizer(ListPoint,Result[1])
+    # for i in range(n) :
+    #     print(ListPoint[i])
+main()
