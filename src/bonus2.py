b57edac3388963a455a6dad584be888153cd0a91
Ariel Jovananda <arieljovananda@Ariels-MacBook-Pro.local>
2023-02-26 19:53:51 +0700

    Functions adjusted for bonus

--- src/bonus2.py
+++ src/bonus2.py
@@ -1,6 +1,7 @@
 import random
 import math
-import ClosestPair as cp
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 count = 0
 def generatePoints(row, col):
@@ -103,10 +104,40 @@ def closestPair(List):
     
     return closestPairRec(sorted)
 
-ListPoint = generatePoints(100,4)
-Result = closestPair(ListPoint)   
-print(Result)
-print('EucDistance count :', count) 
+def visualizer(points, pair): #Visualisasi Grafik Titik-Titik
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    xs = [p[0] for p in points]
+    ys = [p[1] for p in points]
+    zs = [p[2] for p in points]
+    ax.scatter(xs, ys, zs)
+    
+    if pair:
+        x_pair = [pair[0][0], pair[1][0]]
+        y_pair = [pair[0][1], pair[1][1]]
+        z_pair = [pair[0][2], pair[1][2]]
+        ax.scatter(x_pair, y_pair, z_pair, c='red',s=80) #Mengubah warna titik yang merupakan closestPair
+    
+    #Memberi Label
+    ax.set_xlabel('X') 
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    plt.show()
+
+
+row = int(input("Masukkan jumlah point: "))
+col = int(input("Masukkan dimensi: "))
+if col == 3:
+    ListPoint = generatePoints(row,col)
+    Result = closestPair(ListPoint)   
+    print(Result)
+    print('EucDistance count :', count)
+    visualizer(ListPoint, Result[1])
+else:
+    ListPoint = generatePoints(row,col)
+    Result = closestPair(ListPoint)   
+    print(Result)
+    print('EucDistance count :', count) 
 # RunTime = (time.time() - st)
 # print('Execution time:', RunTime, 'seconds') 
 # vis.visualizer(ListPoint,Result[1])
