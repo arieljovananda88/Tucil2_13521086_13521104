c208de7b811223e7edbe38a43cd7c4c8b6201e37
Ariel Jovananda <arieljovananda@Ariels-MacBook-Pro.local>
2023-02-26 19:42:56 +0700

    bonus for now

---
+++ src/bonus2.py
@@ -0,0 +1,126 @@
+import random
+import math
+import ClosestPair as cp
+
+count = 0
+def generatePoints(row, col):
+    matrix = [[random.uniform(0,100) for j in range(col)] for i in range(row)]
+    return matrix
+
+def distance(point1, point2):
+    sum = 0
+    for i in range(len(point1)):
+        sum += ((point1[i] - point2[i]) ** 2)
+    result = math.sqrt(sum)
+    return result
+
+def sortList(mat, axis):
+    if len(mat) <= 1:
+        return mat
+    acuan = mat[0]
+    left = []
+    right = []
+    for i in mat[1:]:
+        if i[axis] < acuan[axis]:
+            left.append(i)
+        else:
+            right.append(i)
+    return sortList(left,axis) + [acuan] + sortList(right,axis)
+
+def bruteForce(List):
+    global count
+    n=len(List)
+    min=float('inf')
+    cp=None
+    
+    for i in range(n):
+        for j in range(i+1,n):
+            temp=distance(List[i],List[j])
+            count+=1
+            if temp < min : 
+                min=temp
+                cp= [List[i],List[j]]
+    return min , cp 
+
+def closestPairRec(List):
+    global count
+    n=len(List)
+    if n<=3 : 
+        return bruteForce(List)
+    
+    mid= n//2
+    midP=List[mid]
+    
+    left_p=List[:mid]
+    right_p=List[mid:]
+    
+    min_l,cp_l=closestPairRec(left_p)   
+    min_r,cp_r=closestPairRec(right_p)
+    
+    minP=min(min_l,min_r)
+    cp=cp_r if min_r<min_l else cp_l
+    unsorted=[]
+    for point in List:
+        if abs(point[0] - midP[0])<minP :
+            unsorted.append(point)
+    
+    sorted = sortList(unsorted, 1)
+    
+    for i in range(len(sorted)):
+        j=i+1
+        while j < len(sorted) and sorted[j][1]-sorted[i][1] < minP :
+            tempMin=distance(sorted[i],sorted[j])
+            count += 1
+            if tempMin < minP:
+                minP=tempMin
+                cp=[sorted[i],sorted[j]]
+            j+=1
+            
+    # sortedZ=[]
+    # for point in sorted :
+    #     if abs(point.y-midP.y)<minP:
+    #         sortedZ.append(point)
+            
+    # sortedZ.sort(key=lambda p: p.z)
+    
+    # for i in range(len(sortedZ)):
+    #     j=j+1
+    #     while j < len(sortedZ) and sortedZ[j].z-sortedZ[i].z < minP :
+    #         tempMin2=distance(sortedZ[i],sortedZ[j])
+    #         if tempMin2 < minP :
+    #             minP=tempMin2
+    #             cp= [sortedZ[i],sortedZ[j]]
+    #         j+=1
+    
+    return minP,cp
+
+def closestPair(List):
+    global count
+    count = 0
+    if len(List)<2 :
+        return None
+    sorted = sortList(List, 0)
+    
+    return closestPairRec(sorted)
+
+ListPoint = generatePoints(100,4)
+Result = closestPair(ListPoint)   
+print(Result)
+print('EucDistance count :', count) 
+# RunTime = (time.time() - st)
+# print('Execution time:', RunTime, 'seconds') 
+# vis.visualizer(ListPoint,Result[1])
+
+# matrix = generatePoints(3,4)
+# for i in range(len(matrix)):
+#     print(matrix[i])
+# sortedMat = sortList(matrix,0)
+# for i in range(len(matrix)):
+#     print(sortedMat[i])
+
+
+
+
+# matrix = generatePoints(5,5)
+# print(matrix[0], matrix[1])
+# print(distance(matrix[0], matrix[1]))
